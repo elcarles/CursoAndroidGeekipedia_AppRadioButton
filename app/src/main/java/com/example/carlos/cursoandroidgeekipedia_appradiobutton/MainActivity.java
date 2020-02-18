c76e64fb6ab1ef198cebcfd8ad817e6250c6f718
elcarles <35566607+elcarles@users.noreply.github.com>
2020-02-18 15:05:02 +0100

    14-02-2020 trabajo

--- app/src/main/java/com/example/carlos/cursoandroidgeekipedia_appradiobutton/MainActivity.java
+++ app/src/main/java/com/example/carlos/cursoandroidgeekipedia_appradiobutton/MainActivity.java
@@ -3,12 +3,26 @@ package com.example.carlos.cursoandroidgeekipedia_appradiobutton;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private EditText et_valor1;
+    private EditText et_valor2;
+    private TextView tv_resultado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        et_valor1 = findViewById(R.id.et_valor1);
+        et_valor2 = findViewById(R.id.et_valor2);
+        tv_resultado = findViewById(R.id.tv_resultado);
+
+
     }
 }

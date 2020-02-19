39fc8b27553a7ef5b08f2f6c69475823f19129ab
elcarles <35566607+elcarles@users.noreply.github.com>
2020-02-19 16:19:11 +0100

    19-02-2020 trabajo

--- app/src/main/java/com/example/carlos/cursoandroidgeekipedia_appradiobutton/MainActivity.java
+++ app/src/main/java/com/example/carlos/cursoandroidgeekipedia_appradiobutton/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText et_valor1 ,et_valor2 ;
     private TextView tv_resultado;
-    private RadioButton rb_sumar, rb_restar;
+    private RadioButton rb_sumar, rb_restar, rb_mult, rb_dividir;
 
 
 
@@ -29,13 +29,15 @@ public class MainActivity extends AppCompatActivity {
         tv_resultado = findViewById(R.id.tv_resultado);
         rb_sumar = findViewById(R.id.rb_sumar);
         rb_restar = findViewById(R.id.rb_restar);
-
+        rb_mult = findViewById(R.id.rb_mult);
+        rb_dividir = findViewById(R.id.rb_dividir);
     }
 
     public void calcular(View view){
 
         String valor1_s = et_valor1.getText().toString();
         String valor2_s = et_valor2.getText().toString();
+
 
         int valor1_int = Integer.parseInt(valor1_s);
         int valor2_int = Integer.parseInt(valor2_s);
@@ -49,12 +51,25 @@ public class MainActivity extends AppCompatActivity {
             tv_resultado.setText(resultado);
         }
         else
-        if (rb_sumar.isChecked()) {
+        if (rb_restar.isChecked()) {
             res = valor1_int - valor2_int;
             String resultado = String.valueOf(res);
             tv_resultado.setText(resultado);
         }
-         else
+        else
+        if (rb_mult.isChecked()) {
+            res = valor1_int * valor2_int;
+            String resultado = String.valueOf(res);
+            tv_resultado.setText(resultado);
+        }
+        else
+        if (rb_dividir.isChecked()) {
+            res = valor1_int - valor2_int;
+            String resultado = String.valueOf(res);
+            tv_resultado.setText(resultado);
+        }
+
+        else
         {Toast.makeText(getApplicationContext(), "Debe de marcar sumar o restar " , Toast.LENGTH_LONG).show();}
 
 
